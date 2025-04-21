d50a9d802c0645e677bbc87e13ca12c474dbb270
Brian Britt <54298777+bmbritt@users.noreply.github.com>
2025-04-20 21:47:32 -0400

    Tree function implementation

--- src/Trees/TreeNode.java
+++ src/Trees/TreeNode.java
@@ -117,6 +117,7 @@ public class TreeNode {
             root.left = buildTree(Arrays.copyOfRange(preorder, 1, mid + 1), Arrays.copyOfRange(inorder, 0, mid));
             root.right = buildTree(Arrays.copyOfRange(preorder, mid + 1, preorder.length), Arrays.copyOfRange(inorder, mid + 1, inorder.length));
 
+            
             return root;
         }
      }
